976a9ed5b1beb22139652b282d7c8867cb2449ea
Dániel Lencsés <daniel.lencses@stud.uni-corvinus.hu>
2020-12-15 00:38:01 +0100

    CSV import

--- IRF_projekt/IRF_projekt/Form1.cs
+++ IRF_projekt/IRF_projekt/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IRF_projekt.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace IRF_projekt
 {
     public partial class Form1 : Form
     {
+        List<Gyerek> gyerekek = new List<Gyerek>();
         public Form1()
         {
             InitializeComponent();
+            betoltes();
+            dataGridView1.DataSource = gyerekek;
         }
+
+        private void betoltes()
+        {
+            using (StreamReader sr = new StreamReader("nevsor.csv", Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(';');
+
+                    Gyerek gyerek = new Gyerek();
+                    gyerek.Name = line[0];
+                    gyerek.Age = Convert.ToInt32(line[1]);
+                    gyerek.Group = line[2];
+                    gyerek.Trip = Convert.ToBoolean(line[3]);
+                    gyerekek.Add(gyerek);
+                }
+            }
+        }
+          
     }
 }
