199d61eebd8e11448d4bd15b947b10caa2659e16
Dániel Lencsés <daniel.lencses@stud.uni-corvinus.hu>
2020-12-14 22:06:29 +0100

    Class

---
+++ IRF_projekt/IRF_projekt/Classes/Gyerek.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_projekt.Classes
+{
+    class Gyerek
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Group { get; set; }
+        public bool Trip { get; set; }
+    }
+}
